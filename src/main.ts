f566669a80d73db7d355738b258872d26682476e
RafaGallardoA <rafael.gallardo@thoughtworks.com>
2022-05-31 12:33:07 -0500

    refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,20 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const configApp = await NestFactory.create(AppModule);
-  let configService = configApp.get(ConfigService);
+  let configService = configApp.get(ConfigService);  
+
+  const microserviceOptions: MicroserviceOptions = {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [configService.get('kafka_broker')],
+      },
+    },
+  };
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(  
     AppModule,
-    {
-      transport: Transport.KAFKA,
-      options: {
-        client: {          
-          brokers: [configService.get('kafka_broker')],                    
-        },
-      },
-    },
+    microserviceOptions
   );
   await app.listen();
 }
